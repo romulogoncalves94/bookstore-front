c626191998071979b10079c280b564df0dc5865d
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-10-18 12:58:21 -0300

    feat: Livro UPDATE

--- src/app/components/views/livro/livro-update/livro-update.component.ts
+++ src/app/components/views/livro/livro-update/livro-update.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-livro-create',
-  templateUrl: './livro-create.component.html',
-  styleUrls: ['./livro-create.component.css']
+  selector: 'app-livro-update',
+  templateUrl: './livro-update.component.html',
+  styleUrls: ['./livro-update.component.css']
 })
-export class LivroCreateComponent implements OnInit {
+export class LivroUpdateComponent implements OnInit {
 
   id_cat: String = ''
 
@@ -28,15 +28,23 @@ export class LivroCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+    this.livro.id = this.route.snapshot.paramMap.get('id')!
+    this.findById()
   }
 
-  create(): void {
-    this.service.create(this.livro, this.id_cat).subscribe((resposta) => {
+  findById(): void {
+    this.service.findById(this.livro.id!).subscribe((resposta) => {
+      this.livro = resposta
+    })
+  }
+
+  update(): void {
+    this.service.update(this.livro).subscribe((resposta) => {
       this.router.navigate([`categorias/${this.id_cat}/livros`])
-      this.service.mensagem('Livro criado com sucesso!')
+      this.service.mensagem('Livro atualizado com sucesso!')
     }, err => {
       this.router.navigate([`categorias/${this.id_cat}/livros`])
-      this.service.mensagem('Erro ao criar um novo livro! Tente novamente mais tarde!')
+      this.service.mensagem('Falha ao atualizar livro! Tente mais tarde!')
     })
   }
 
