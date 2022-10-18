6e034b7d8a07fba242780c7dc314a6319283b3f0
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-10-18 10:56:19 -0300

    feat: Categoria UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'categorias/delete/:id',
     component: CategoriaDeleteComponent
+  },
+  {
+    path: 'categorias/update/:id',
+    component: CategoriaUpdateComponent
   }
 ];
 
